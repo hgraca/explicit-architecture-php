105e85d7aa56115cb4c909a11d7c7e3a036ff116
Herberto Graca <herberto.graca@gmail.com>
2017-12-25 19:59:26 +0000

    Refactor the GenericEvent/subscriber into CommentCreatedEvent & Listener
    An event subscriber will react to several events and, as far as I'm
     concerned, thi means it is breaking the Single Responsibility
     Principle. If we have several event subscribers, how do we know which
     subscriber is reacting to which event? My experience tells me this
     ends up becoming a mess.
    
    An event listener will listen to only one event, although it can have
     several reactions to that event, for example a method to send out an
     email and another method to change some flag.
    
    Furthermore, each component will have (at most) one event listener for
     each event, and each event might be triggered by the same component
     or any other component.
    
    The event being triggered here is an ApplicationEvent, which means that
     a business use case has been executed, so I placed it in the
     application layer under the Event namespace.
    
    However, the event will be triggered from one component but it will
     likely be listened to by other components. This means that components
     will have dependencies on each other because of the events. But we
     want the components to be decoupled among them, so we place the event
     in a SharedKernel namespace, so that this way the components are
     coupled to the SharedKernel but are decoupled among them. In this case,
     the SharedKernel only has this event, but it could contain any class
     that needs to be shared among components, like Specification objects.
     If at some point we decide to extract a component into a micro-service,
     we just need to extract the SharedKernel into its own repository and
     install it in both application servers using a dependency management
     tool like Composer (php) or Maven (java). If our micro-services are
     built using different languages, then this SharedKernel needs to be
     built using a descriptive and language agnostic fashion so that each
     different application using it can generate its own classes from those
     descriptions. For example, we would place in the SharedKernel a
     description of the event in JSON, and during deployment we would
     generate the event class in the programming language that the micro-service would use.

--- src/Core/Component/Blog/Application/Service/CommentService.php
+++ src/Core/Component/Blog/Application/Service/CommentService.php
@@ -17,7 +17,9 @@ namespace Acme\App\Core\Component\Blog\Application\Service;
 use Acme\App\Core\Component\Blog\Domain\Entity\Comment;
 use Acme\App\Core\Component\Blog\Domain\Entity\Post;
 use Acme\App\Core\Component\User\Domain\Entity\User;
+use Acme\App\Core\SharedKernel\Component\Blog\Application\Event\CommentCreatedEvent;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 final class CommentService
 {
@@ -26,9 +28,15 @@ final class CommentService
      */
     private $entityManager;
 
-    public function __construct(ObjectManager $entityManager)
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    public function __construct(ObjectManager $entityManager, EventDispatcherInterface $eventDispatcher)
     {
         $this->entityManager = $entityManager;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function create(Post $post, Comment $comment, User $author): void
@@ -41,5 +49,19 @@ final class CommentService
         // Flushing is when doctrine writes all staged changes, to the DB
         // so we should do this only once in a request, and only if the use case command was successful
         $this->entityManager->flush(); // if we would use a command bus, we would do this in a middleware
+
+        // When triggering an event, you can optionally pass some information.
+        // For simple applications, use the GenericEvent object provided by Symfony
+        // to pass some PHP variables. For more complex applications, define your
+        // own event object classes.
+        // See https://symfony.com/doc/current/components/event_dispatcher/generic_event.html
+        $event = new CommentCreatedEvent($comment);
+
+        // When an event is dispatched, Symfony notifies it to all the listeners
+        // and subscribers registered to it. Listeners can modify the information
+        // passed in the event and they can even modify the execution flow, so
+        // there's no guarantee that the rest of this method will be executed.
+        // See https://symfony.com/doc/current/components/event_dispatcher.html
+        $this->eventDispatcher->dispatch(CommentCreatedEvent::class, $event);
     }
 }
---
+++ src/Core/SharedKernel/Component/Blog/Application/Event/CommentCreatedEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Explicit Architecture POC,
+ * which is created on top of the Symfony Demo application.
+ *
+ * (c) Herberto Graça <herberto.graca@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acme\App\Core\SharedKernel\Component\Blog\Application\Event;
+
+use Acme\App\Core\Component\Blog\Domain\Entity\Comment;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * This is just a DTO, it only has getters, theres no logic to test, so we ignore it for code coverage purposes.
+ *
+ * @codeCoverageIgnore
+ */
+final class CommentCreatedEvent extends Event
+{
+    /**
+     * @var Comment
+     */
+    private $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getComment(): Comment
+    {
+        return $this->comment;
+    }
+}
