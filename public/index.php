e9a4677c8ad34db510ea6c876408369aa150421c
Herberto Graca <herberto.graca@gmail.com>
2018-03-18 22:59:42 +0100

    bugfix reading environment variables from $_SERVER instead of $_ENV

--- public/index.php
+++ public/index.php
@@ -17,11 +17,11 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
+if (!isset($_ENV['APP_ENV'])) {
     (new Dotenv())->load(__DIR__.'/../.env');
 }
 
-if ($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))) {
+if ($_ENV['APP_DEBUG'] ?? ('prod' !== ($_ENV['APP_ENV'] ?? 'dev'))) {
     umask(0000);
 
     Debug::enable();
@@ -29,7 +29,7 @@ if ($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))) {
 
 // Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
 
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', $_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev')));
+$kernel = new Kernel($_ENV['APP_ENV'] ?? 'dev', $_ENV['APP_DEBUG'] ?? ('prod' !== ($_ENV['APP_ENV'] ?? 'dev')));
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
