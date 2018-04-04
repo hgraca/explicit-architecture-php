80ef2efade003920b0c26434e1f7b72004cb718e
Herberto Graca <herberto.graca@gmail.com>
2018-04-04 22:39:54 +0200

    Update the files header

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the Explicit Architecture POC,
+ * which is created on top of the Symfony Demo application.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Herberto Graça <herberto.graca@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
