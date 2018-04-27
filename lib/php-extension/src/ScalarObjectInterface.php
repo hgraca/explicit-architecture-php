922bffb19b3eab66bd967cdaf7558102f6097ccd
Herberto Graca <herberto.graca@gmail.com>
2018-04-27 20:42:56 +0200

    Add EqualityInterface and ScalarObjectInterface

---
+++ lib/php-extension/src/ScalarObjectInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Explicit Architecture POC,
+ * which is created on top of the Symfony Demo application.
+ *
+ * (c) Herberto Graça <herberto.graca@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acme\PhpExtension;
+
+interface ScalarObjectInterface
+{
+    public function toScalar();
+}
