07b66c1c418e826697ecab6f6a672c4e5021a91c
Herberto Graca <herberto.graca@gmail.com>
2019-04-01 23:02:22 +0200

    [WIP] [tests missing] Create PostRepositoryInterface::findAllByUserId

--- src/Core/Component/Blog/Application/Repository/PostRepositoryInterface.php
+++ src/Core/Component/Blog/Application/Repository/PostRepositoryInterface.php
@@ -17,6 +17,7 @@ namespace Acme\App\Core\Component\Blog\Application\Repository;
 use Acme\App\Core\Component\Blog\Domain\Post\Post;
 use Acme\App\Core\Component\Blog\Domain\Post\PostId;
 use Acme\App\Core\Port\Persistence\ResultCollectionInterface;
+use Acme\App\Core\SharedKernel\Component\User\Domain\User\UserId;
 
 interface PostRepositoryInterface
 {
@@ -24,6 +25,15 @@ interface PostRepositoryInterface
      * @return Post[]
      */
     public function findAll(array $orderByList = ['id' => 'DESC'], int $maxResults = null): ResultCollectionInterface;
+
+    /**
+     * @return Post[]
+     */
+    public function findAllByUserId(
+        UserId $userId,
+        array $orderByList = ['publishedAt' => 'DESC'],
+        int $maxResults = null
+    ): ResultCollectionInterface;
 
     public function find(PostId $id): Post;
 
