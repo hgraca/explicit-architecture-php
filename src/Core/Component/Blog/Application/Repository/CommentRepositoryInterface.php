4c72320f283a712c42b29df515c6692a8b8782d4
Herberto Graca <herberto.graca@gmail.com>
2019-04-08 22:45:20 +0200

    [WIP] [tests missing] Create the CommentRepository

---
+++ src/Core/Component/Blog/Application/Repository/CommentRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Explicit Architecture POC,
+ * which is created on top of the Symfony Demo application.
+ *
+ * (c) Herberto Graça <herberto.graca@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acme\App\Core\Component\Blog\Application\Repository;
+
+use Acme\App\Core\Component\Blog\Domain\Post\Comment\Comment;
+use Acme\App\Core\Component\Blog\Domain\Post\PostId;
+use Acme\App\Core\Port\Persistence\ResultCollectionInterface;
+
+interface CommentRepositoryInterface
+{
+    /**
+     * @return Comment[]
+     */
+    public function findAllByPostId(
+        PostId $postId,
+        array $orderByList = ['publishedAt' => 'DESC'],
+        int $maxResults = null
+    ): ResultCollectionInterface;
+}
