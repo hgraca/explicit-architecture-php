47df613726971ea56910fc313beb517309bad641
Herberto Graca <herberto.graca@gmail.com>
2019-04-13 14:03:27 +0200

    Remove dead code from AccessDeniedExceptionSubscriber

--- src/Infrastructure/Auth/Symfony/AccessDeniedExceptionSubscriber.php
+++ src/Infrastructure/Auth/Symfony/AccessDeniedExceptionSubscriber.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace Acme\App\Infrastructure\Auth\Symfony;
 
 use Acme\App\Core\Port\Auth\AccessDeniedException;
-use Acme\App\Core\Port\Persistence\TransactionServiceInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -23,16 +22,6 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException as SymfonyAc
 
 final class AccessDeniedExceptionSubscriber implements EventSubscriberInterface
 {
-    /**
-     * @var TransactionServiceInterface
-     */
-    private $transactionService;
-
-    public function __construct(TransactionServiceInterface $transactionService)
-    {
-        $this->transactionService = $transactionService;
-    }
-
     /**
      * Return the subscribed events, their methods and possibly their priorities
      * (the higher the priority the earlier the method is called).
